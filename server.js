570564e9bdba3875a375f73b9e30fcaedf644b4c
MrTofYfY <vodoleevich10@mail.ru>
2026-02-19 09:12:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -144,8 +144,9 @@ app.post('/send-data', (req, res) => {
     res.json({ success: true });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 
 });
+
